17c8ec157838c2a929b37918acf5e3329fafd156
EstLopCor <estherlopezcordoncillo@gmail.com>
2022-06-10 12:45:31 +0200

    Change favicon

--- src/components/Personaje.js
+++ src/components/Personaje.js
@@ -1,21 +1,2 @@
-function Personaje(personajeInfo) {
-  return (
-    <div className="col-3">
-      <div className="card">
-        <img
-          src={personajeInfo.image}
-          alt={personajeInfo.name}
-          className="card-img-top"
-        />
-        <div className="card-body">
-          {" "}
-          <h3 className="card-title">{personajeInfo.name}</h3>
-          <p className="overflow-hidden">{`Origin${
-            personajeInfo.origin && personajeInfo.origin.name
-          }`}</p>
-        </div>
-      </div>
-    </div>
-  );
-}
+function Personaje() {}
 export default Personaje;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,9 @@
 import ReactDOM from "react-dom/client";
-import "bootstrap/dist/css/bootstrap.min.css";
 import List from "./components/List";
 
 function App() {
   return (
     <div className="container">
-      <nav className="navbar sticky-top navbar-light bg-dark">
-        <h1 className="navbar-brand text-light">Rick and Morty</h1>
-      </nav>
       <List />
     </div>
   );
